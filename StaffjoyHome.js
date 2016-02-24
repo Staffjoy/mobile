166c2043ee0e5fe4d8abef8ca0a8f6bb99f6868c
Jonathan Ballerano <jballer@jballer.com>
2016-02-24 00:37:39 -0800

    Able to identify and back away from external links

--- StaffjoyHome.js
+++ StaffjoyHome.js
@@ -1,13 +1,58 @@
 import React, {
+  Alert,
   Component,
+  // Linking,
+  Platform,
   StyleSheet,
   Text,
   View,
-  WebView,
-  Platform
+  WebView
 } from 'react-native';
 
+function runOnEachPage() {
+
+  var url = document.location.href;
+
+  var REDIRECTS = {
+    '/': '/auth/native',
+    '/auth/login': '/auth/native',
+  };
+
+  if (url.startsWith(this.state.baseURL)) {
+    let path = url.substring(this.state.baseURL.length);
+    let redirect = REDIRECTS[path]
+    if (redirect !== undefined) {
+      document.location.href = redirect;
+    }
+  }
+  else {
+    window.history.back();
+    url;
+  }
+}
+
 var StaffjoyHome = React.createClass({
+
+  getInitialState() {
+    var baseURL;
+    let defaultPath = '/auth/native';
+
+    if (this.props.baseURL) {
+      // remove trailing slash if present
+      baseURL = this.props.baseURL.replace(/\/?$/, '');
+    }
+    else {
+      baseURL = 'https://www.staffjoy.com';
+    }
+
+    let url = baseURL + defaultPath;
+
+    return {
+      baseURL: baseURL,
+      defaultPath: defaultPath,
+      url: url
+    }
+  },
 
   render() {
     return (
@@ -16,10 +61,30 @@ var StaffjoyHome = React.createClass({
         <WebView
           url={this.props.source}
           style={styles.web}
+          onLoad={this.onLoad}
+          injectedJavaScript={this.javaScriptToInject()}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
         />
       </View>
     );
+  },
+
+  javaScriptToInject() {
+    let fn = runOnEachPage.toString();
+    fn = fn.replace(RegExp('this.state.baseURL', 'g'), '\'' + this.state.baseURL + '\'');
+    fn = fn.replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'');
+    // fn += '\nrunOnEachPage();';
+    return fn;
+  },
+
+  onLoad(event) {
+    let url = event.nativeEvent.jsEvaluationValue;
+    if (url !== undefined && url !== '')
+    {
+      Alert.alert('External URL', 'I should be opening ' + url + ' in an external browser right about now!');
+    }
   }
+
 });
 
 var styles = StyleSheet.create({
