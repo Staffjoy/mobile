5ba42dfdb9f5ce78037f8d9e267a50aefc8514dc
Jonathan Ballerano <jballer@jballer.com>
2016-03-01 00:42:33 -0800

    Rename activities since React explicitly uses `MainActivity` as entry point

--- android/app/src/main/java/com/staffjoymobile/MainActivity.java
+++ android/app/src/main/java/com/staffjoymobile/MainActivity.java
@@ -1,40 +1,17 @@
 package com.staffjoymobile;
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-import java.util.Arrays;
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
     @Override
-    protected String getMainComponentName() {
-        return "StaffjoyMobile";
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-   /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
-      );
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
